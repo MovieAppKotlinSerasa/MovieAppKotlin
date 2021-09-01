8c6c8b40f30f4b9772fa7168365539f023a987b9
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-09-01 09:42:12 -0300

    Git flow init

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.container, MainFragment.newInstance())
-                
+
             .commitNow()
     }
 }
