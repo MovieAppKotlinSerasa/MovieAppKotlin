4ae9a4f1ecefb0d44ab8a6f4d705d0cfb3b57541
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-09-01 10:09:54 -0300

    ID-7 Test

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -13,5 +13,6 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.container, MainFragment.newInstance())
             .commitNow()
+        
     }
 }
