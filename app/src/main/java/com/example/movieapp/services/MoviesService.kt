dc3bbb5c5879e315cbd04018e9dd447dafc9f0cd
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-09-12 22:41:58 -0300

    ID-9 Creating dynamic parameters to endpoints

--- app/src/main/java/com/example/movieapp/services/MoviesService.kt
+++ app/src/main/java/com/example/movieapp/services/MoviesService.kt
@@ -1,23 +1,37 @@
 package com.example.movieapp.services
 
 import com.example.movieapp.BuildConfig
-import com.example.movieapp.model.MoviesData
-import retrofit2.Call
+import com.example.movieapp.model.Genres
+import com.example.movieapp.model.Movies
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MoviesService {
 
     @GET("movie/popular")
-    fun getMostPopularMovies(
+    suspend fun getMostPopularMovies(
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
-        @Query("page") page: Int
-    ): Call<MoviesData>
+        @Query("language") language: String = "pt-BR",
+        @Query("page") page: Int,
+        @Query("with_genres") genres: String = "12,16",
+        /** to do -> val numbers = listOf(1, 2, 3, 4, 5, 6)
+         * numbers.joinToString() // 1, 2, 3, 4, 5, 6
+         */
+    ): Response<Movies>
+
+    @GET("/search/movie")
+    suspend fun getFilteredMoviesByName(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "pt-BR",
+        @Query("query") title: String,
+        @Query("page") page: Int,
+    ): Response<Movies>
 
     @GET("genre/movie/list")
-    fun getAllGenres(
+    suspend fun getAllGenres(
         @Query("api_key") apiKey: String = BuildConfig.API_KEY
-    )
+    ): Response<Genres>
 
 
 
