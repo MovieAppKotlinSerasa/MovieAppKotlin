6211b84aa514e0a9e1daf6dbaf0e3925e2315036
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-09-12 22:48:09 -0300

    FavoritesMovies dao constructed

--- app/src/main/java/com/example/movieapp/database/dao/FavoritesMoviesDAO.kt
+++ app/src/main/java/com/example/movieapp/database/dao/FavoritesMoviesDAO.kt
@@ -1,10 +1,27 @@
 package com.example.movieapp.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.movieapp.model.FavoritesMovies
 
 @Dao
 interface FavoritesMoviesDAO {
 
+    @Insert
+    fun insertFavoriteMovie(favoritesMovies: FavoritesMovies)
 
+    @Query("SELECT * FROM FavoritesMovies WHERE movie_title LIKE '%' || :title || '%'")
+    fun fetchFilteredFavoritesMovies(title: String): List<FavoritesMovies>
+
+    @Query("SELECT * FROM FavoritesMovies")
+    fun fetchAllFavoritesMovies(): List<FavoritesMovies>
+
+    @Delete
+    fun deleteOneFavoriteMovie(favoritesMovies: FavoritesMovies)
+
+    @Delete
+    fun deleteAllFavoritesMovies(allFavoritesMovies: List<FavoritesMovies>)
 
 }
