b0ab50f9b7402440d1af20c47815672ab7adb487
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-09-12 22:50:16 -0300

    Hotfix - signout button return to login screen without any data

--- app/src/main/java/com/example/movieapp/LoginActivity.kt
+++ app/src/main/java/com/example/movieapp/LoginActivity.kt
@@ -1,18 +1,36 @@
 package com.example.movieapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.movieapp.view.MovieFragment
+import androidx.appcompat.app.AppCompatActivity
+import com.example.movieapp.utils.replaceView
+import com.example.movieapp.view.LoginFragment
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MovieFragment.newInstance())
-                .commitNow()
+
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            startHomeActivity()
+        } else {
+            replaceView(LoginFragment.newInstance(), R.id.container)
         }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            replaceView(LoginFragment.newInstance(), R.id.container)
+        }
+    }
+
+    private fun startHomeActivity(){
+        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
