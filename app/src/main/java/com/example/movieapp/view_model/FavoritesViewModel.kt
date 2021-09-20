7b9d486e9b48bed962b35971991682f0523c4f71
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-09-20 16:04:15 -0300

    Inserted room functions to save online data to local database

--- app/src/main/java/com/example/movieapp/view_model/FavoritesViewModel.kt
+++ app/src/main/java/com/example/movieapp/view_model/FavoritesViewModel.kt
@@ -5,22 +5,29 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.movieapp.model.Movie
+import com.example.movieapp.repository.AuthenticationRepository
 import com.example.movieapp.repository.FavoritesRepository
+import com.example.movieapp.repository.MoviesRepository
+import com.example.movieapp.repository.OfflineFavoritesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FavoritesViewModel @Inject constructor(
-    private val favoritesRepository: FavoritesRepository
+    private val authRepository: AuthenticationRepository,
+    private val repository: MoviesRepository,
+    private val favoritesRepository: FavoritesRepository,
+    private val offlineRepository: OfflineFavoritesRepository
 ) : ViewModel() {
 
     private val _movies = MutableLiveData<List<Movie>>()
     val movies: LiveData<List<Movie>> = _movies
 
     fun removeMovie(id: Long?) {
-        viewModelScope.launch {
-            if (id != null) {
+        if (id != null) {
+            removeLocalFavs(id)
+            viewModelScope.launch {
                 favoritesRepository.removeFavorite(id.toLong()) {
                     getMovies()
                 }
@@ -28,9 +35,22 @@ class FavoritesViewModel @Inject constructor(
         }
     }
 
+    private fun removeLocalFavs(id: Long) {
+        val currentUserEmail = authRepository.currentUser()?.email
+        if (!currentUserEmail.isNullOrEmpty()) {
+            viewModelScope.launch {
+                val movie = repository.getMovieById(id)
+                if (movie != null) {
+                    offlineRepository.deleteFavMovie(currentUserEmail, movie.id)
+                    println()
+                }
+            }
+        }
+    }
+
     fun getMovies() {
         viewModelScope.launch {
-            favoritesRepository.getAllMoviesFromFirebase{ bills, error ->
+            favoritesRepository.getAllMoviesFromFirebase { bills, error ->
                 _movies.value = bills
             }
         }
--- app/src/main/java/com/example/movieapp/view_model/MovieViewModel.kt
+++ app/src/main/java/com/example/movieapp/view_model/MovieViewModel.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.movieapp.model.Movie
 import com.example.movieapp.model.MovieResult
-import com.example.movieapp.repository.AuthenticationRepository
-import com.example.movieapp.repository.FavoritesRepository
-import com.example.movieapp.repository.MoviesRepository
+import com.example.movieapp.model.User
+import com.example.movieapp.repository.*
 import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -17,8 +17,10 @@ import javax.inject.Inject
 class MovieViewModel @Inject constructor(
     private val authRepository: AuthenticationRepository,
     private val moviesRepository: MoviesRepository,
-    private val favoritesRepository: FavoritesRepository
-    ) : ViewModel() {
+    private val favoritesRepository: FavoritesRepository,
+    private val offlineRepository: OfflineFavoritesRepository,
+    private val usersRepository: UsersRepository
+) : ViewModel() {
 
     private val _movies = MutableLiveData<MovieResult>()
     val movieResult: LiveData<MovieResult> = _movies
@@ -27,19 +29,58 @@ class MovieViewModel @Inject constructor(
     val currentUser: LiveData<FirebaseUser> = _currentUser
 
     fun fetchCurrentUser() {
-        authRepository.currentUser().apply {
+        authRepository.currentUser()?.apply {
             _currentUser.value = this
+            insertNewLoggedUser(this.email!!)
         }
     }
 
     fun getMovies() {
-
+        fetchLocalFavs()
         viewModelScope.launch {
-            moviesRepository.getAllMoviesFromService(1, listOf(12,16))?.let { movies ->
+            moviesRepository.getAllMoviesFromService(1, listOf(12, 16))?.let { movies ->
                 _movies.value = movies
 //                val listOfIds = movies.results.map { it.id }
 //                favoritesRepository.addFavorite(listOfIds)
             }
         }
     }
+
+    private fun fetchLocalFavs() {
+        val currentUserEmail = authRepository.currentUser()?.email
+        if (!currentUserEmail.isNullOrEmpty()) {
+            favoritesRepository.getAllMoviesFromFirebase { movies, error ->
+                if (movies != null) {
+                    updateLocalFavList(currentUserEmail, movies)
+                }
+            }
+
+        }
+    }
+
+    private fun updateLocalFavList(currentUserEmail: String, movies: List<Movie>) {
+        movies.forEach { Movie ->
+            Movie.userEmail = currentUserEmail
+        }
+        viewModelScope.launch {
+            offlineRepository.clearFavList(currentUserEmail)
+            offlineRepository.insertNewFavList(movies)
+        }
+    }
+
+    private fun insertNewLoggedUser(userEmail: String) {
+        viewModelScope.launch {
+            val users = usersRepository.fetchUsers()
+            var doesNotContainsUserEmail = true
+            users.forEach {
+                if(it.userEmail == userEmail) {
+                    doesNotContainsUserEmail = false
+                }
+            }
+            if(doesNotContainsUserEmail) {
+                usersRepository.insertUser(userEmail)
+            }
+        }
+    }
+
 }
