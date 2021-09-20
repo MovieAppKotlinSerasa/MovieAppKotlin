1cbfd272ea133797d33e27c01d928179959e3e32
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-09-20 16:06:31 -0300

    Removed unused functions

--- app/src/main/java/com/example/movieapp/model/MovieResult.kt
+++ app/src/main/java/com/example/movieapp/model/MovieResult.kt
@@ -1,5 +1,9 @@
 package com.example.movieapp.model
 
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.QueryDocumentSnapshot
 
@@ -8,49 +12,59 @@ data class MovieResult(
     val results: List<Movie>
 )
 
+@Entity
 data class Movie(
-
-    var id: Long,
+    @PrimaryKey
+    var pkMovie: Long? = 0,
+    var userEmail: String? = null,
+    val id: Long,
     val title: String,
     val overview: String,
     val poster_path: String,
     val vote_average: Float,
+//    @Embedded
     val genres: List<Genre>?
 
 ) {
-    companion object {
-
-        fun fromData(snapshot: QueryDocumentSnapshot): Movie {
-
-            return Movie(
-                id = snapshot.id.toLong(),
-                title = snapshot.data["original_title"] as String,
-                overview = snapshot.data["overview"] as String,
-                poster_path = snapshot.data["posterPath"] as String,
-                vote_average = snapshot.data["vote_average"] as Float,
-                genres = snapshot.data["genres"] as List<Genre>
-            )
-
-        }
-
-        fun fromDocument(doc: DocumentSnapshot): Movie {
-
-            return Movie(
-                id = doc.id.toLong(),
-                title = doc["original_title"] as String,
-                overview = doc["overview"] as String,
-                poster_path = doc["posterPath"] as String,
-                vote_average = doc["vote_average"] as Float,
-                genres = doc["genres"] as List<Genre>
-            )
-
-        }
-
-    }
+//    constructor(): this( "", 0L, "", "", "", 0F, listOf<Genre>())
+//    companion object {
+//
+//        fun fromData(snapshot: QueryDocumentSnapshot): Movie {
+//
+//            return Movie(
+//                id = snapshot.id.toLong(),
+//                title = snapshot.data["original_title"] as String,
+//                overview = snapshot.data["overview"] as String,
+//                poster_path = snapshot.data["posterPath"] as String,
+//                vote_average = snapshot.data["vote_average"] as Float,
+//                genres = snapshot.data["genres"] as List<Genre>
+//            )
+//
+//        }
+//
+//        fun fromDocument(doc: DocumentSnapshot): Movie {
+//
+//            return Movie(
+//                id = doc.id.toLong(),
+//                title = doc["original_title"] as String,
+//                overview = doc["overview"] as String,
+//                poster_path = doc["posterPath"] as String,
+//                vote_average = doc["vote_average"] as Float,
+//                genres = doc["genres"] as List<Genre>
+//            )
+//
+//        }
+//
+//    }
 
 }
 
+@Entity
 data class Genre(
+    @PrimaryKey(autoGenerate = true)
+    var pkGenre: Long? = 0,
     val id: Int,
     val name: String
-)
+) {
+//    constructor(): this(0, 0, "")
+}
